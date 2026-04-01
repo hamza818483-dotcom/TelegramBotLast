72ad715581a79c4ab4fe3ab490241c98f6c08b60
Amir Hamza Rafi <hamza818483@gmail.com>
2026-04-01 11:57:53 +0600

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+TELEGRAM_BOT_TOKEN = "8297282007:AAGZTiGNGuMaEVjjpF1gNO3VnrL0bAbYfGc"
+
+# Super admin ID
+SUPER_ADMINS = [5341425626]  # নিজের ID দিয়ে replace করুন
+
+# Temp directory for storing files
+TEMP_DIR = "temp_files"
+
+# CSV output folder
+CSV_DIR = "csv_files"
